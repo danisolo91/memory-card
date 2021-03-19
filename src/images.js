08cce024b11f94ff56b47e3685c77e70f1e370f2
Daniel <danisolo91@gmail.com>
2021-03-19 23:13:31 +0100

    Delete images.js

--- src/images.js
+++
@@ -1,8 +0,0 @@
-let images = ['ant','bear','bee',
-                         'bird','ant','camel',
-                         'cat','caterpillar','chicken',
-                         'cow','crab','crocodile',
-                         'deer','dog','dolphin',
-                         'donkey','fish','giraffe'];
-
-export default images;
